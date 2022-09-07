3efd3bfe15607c4f6c1f99b57cbb5259af34bc65
Lassi Piironen <lsipii@kapsi.fi>
2022-09-07 12:28:28 +0300

    build: force new deploy

--- src/api-docs.ts
+++ src/api-docs.ts
@@ -1,14 +1,14 @@
 import { APIGatewayProxyEventV2 } from "aws-lambda";
 
 const swaggerUi = require("aws-serverless-swagger-ui");
-const swaggerHandler = swaggerUi.setup("./openapi/authentication-gw.yml");
+const swagger = swaggerUi.setup("./openapi/authentication-gw.yml");
 
 export const handler = async (event: APIGatewayProxyEventV2) => {
   let eventPath = event.rawPath;
   if (eventPath === "/docs" || eventPath === "/docs/") {
     eventPath = "/docs/index.html";
   }
-  return (await swaggerHandler)({
+  return (await swagger)({
     path: eventPath,
   });
 };
