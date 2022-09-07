76998add2a5796728b42d1e36fa83275147ec706
Lassi Piironen <lsipii@kapsi.fi>
2022-09-07 11:48:13 +0300

    build: revert openapi path, add debug

--- src/api-docs.ts
+++ src/api-docs.ts
@@ -3,7 +3,11 @@ import { APIGatewayProxyEventV2 } from "aws-lambda";
 const swaggerUi = require("aws-serverless-swagger-ui");
 const swaggerHandler = swaggerUi.setup("./openapi/authentication-gw.yml");
 
+const fsPromises = require("fs/promises");
+
 export const handler = async (event: APIGatewayProxyEventV2) => {
+  const files = await fsPromises.readdir("./");
+  console.log(files);
   let eventPath = event.rawPath;
   if (eventPath === "/docs") {
     eventPath = "/docs/index.html";
