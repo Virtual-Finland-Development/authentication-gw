be46cae11f9a90758de494197c36fc7d816d2c29
Lassi Piironen <lsipii@kapsi.fi>
2022-09-11 20:16:48 +0300

    build: debug the sed more

--- scripts/fix-example-index-url.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-echo "DEBUG: $AUTH_PROVIDER_REDIRECT_BACK_HOST"
-ls -la ./web/index.html
-sed -i "s/authAPIHost: \"\",/authAPIHost: \"${AUTH_PROVIDER_REDIRECT_BACK_HOST}\",/g" ./web/index.html
