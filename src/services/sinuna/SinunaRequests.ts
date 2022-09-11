c3751dcc4dc493ab68ea5c4f783bd8cb12431f8e
Lassi Piironen <lsipii@kapsi.fi>
2022-09-11 18:34:26 +0300

    debug: check live logout flow with defaults

--- src/services/sinuna/SinunaRequests.ts
+++ src/services/sinuna/SinunaRequests.ts
@@ -57,8 +57,8 @@ export async function parseAuthenticateResponse(queryParams: { [key: string]: st
 export async function getLogoutRequestUrl(appContext: AppContext): Promise<string> {
   await initializeSinunaRequests();
   // Redirecting straight to the app context might work, but not tested
-  const logoutRedirectUrl = prepareLogoutRedirectUrl(appContext.redirectUrl);
-  return `https://login.iam.qa.sinuna.fi/oxauth/restv1/end_session?post_logout_redirect_uri="${logoutRedirectUrl}"`;
+  //const logoutRedirectUrl = prepareLogoutRedirectUrl(appContext.redirectUrl);
+  return `https://login.iam.qa.sinuna.fi/oxauth/restv1/end_session`;
 }
 
 /**
