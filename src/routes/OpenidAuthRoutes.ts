354252909e9fa4bc5d3990e2dc604584235c3ae1
Lassi Piironen <lsipii@kapsi.fi>
2022-09-29 22:58:29 +0300

    fix: add missing handler

--- src/routes/OpenidAuthRoutes.ts
+++ src/routes/OpenidAuthRoutes.ts
@@ -3,6 +3,6 @@ import { generateRequestHandlers } from "../utils/route-utils";
 
 const defaultAuthProviderIdent = SinunaSettings.ident;
 const operationPrefix = "OpenId";
-const operationNames = ["LoginRequest", "AuthenticateResponse", "AuthTokenRequest", "LogoutRequest", "UserInfoRequest", "AuthorizeRequest"];
+const operationNames = ["LoginRequest", "AuthenticateResponse", "AuthTokenRequest", "LogoutRequest", "LogoutResponse", "UserInfoRequest", "AuthorizeRequest"];
 
 export default generateRequestHandlers(operationNames, operationPrefix, defaultAuthProviderIdent);
--- src/routes/Saml2AuthRoutes.ts
+++ src/routes/Saml2AuthRoutes.ts
@@ -3,6 +3,6 @@ import { generateRequestHandlers } from "../utils/route-utils";
 
 const defaultAuthProviderIdent = SuomiFISettings.ident;
 const operationPrefix = "Saml2";
-const operationNames = ["LoginRequest", "AuthenticateResponse", "LogoutRequest", "UserInfoRequest"];
+const operationNames = ["LoginRequest", "AuthenticateResponse", "LogoutRequest", "LogoutResponse", "UserInfoRequest"];
 
 export default generateRequestHandlers(operationNames, operationPrefix, defaultAuthProviderIdent);
