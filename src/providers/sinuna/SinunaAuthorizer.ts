13ddcd8ce906124055b7aa6e6218d71888a25080
Lassi Piironen <lsipii@kapsi.fi>
2022-10-03 14:16:01 +0300

    fix: switch implementations

--- src/providers/sinuna/SinunaAuthorizer.ts
+++ src/providers/sinuna/SinunaAuthorizer.ts
@@ -1,9 +1,10 @@
 import axios from "axios";
 import { AccessDeniedException } from "../../utils/exceptions";
 import { debug, logAxiosException } from "../../utils/logging";
+import { leftTrim } from "../../utils/transformers";
 
 export default async function authorize(token: string, context: string): Promise<void> {
-  const accessToken = token;
+  const accessToken = leftTrim(token, "Bearer ");
 
   try {
     const response = await axios.get(`https://login.iam.qa.sinuna.fi/oxauth/restv1/userinfo`, {
