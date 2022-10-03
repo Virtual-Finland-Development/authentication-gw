9f263733307aff6d2a5f8f218a1235c2203e3afa
Lassi Piironen <lsipii@kapsi.fi>
2022-10-03 14:44:04 +0300

    fix: drop the refactored route

--- src/routes/OpenidAuthRoutes.ts
+++ src/routes/OpenidAuthRoutes.ts
@@ -3,18 +3,6 @@ import { generateRequestHandlers } from "../utils/route-utils";
 
 const defaultAuthProviderIdent = SinunaSettings.ident;
 const operationPrefix = "OpenId";
-const operationNames = [
-  "LoginRequest",
-  "AuthenticateResponse",
-  "AuthTokenRequest",
-  "LogoutRequest",
-  "LogoutResponse",
-  "UserInfoRequest",
-  "AuthorizeRequest",
-];
+const operationNames = ["LoginRequest", "AuthenticateResponse", "AuthTokenRequest", "LogoutRequest", "LogoutResponse", "UserInfoRequest"];
 
-export default generateRequestHandlers(
-  operationNames,
-  operationPrefix,
-  defaultAuthProviderIdent
-);
+export default generateRequestHandlers(operationNames, operationPrefix, defaultAuthProviderIdent);
