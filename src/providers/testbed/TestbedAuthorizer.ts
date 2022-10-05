b4c93538aebf048849c4a35bb1a3c104500145a3
Lassi Piironen <lsipii@kapsi.fi>
2022-10-05 16:01:40 +0300

    feat: verify issuer pre jwsk fetch

--- src/providers/testbed/TestbedAuthorizer.ts
+++ src/providers/testbed/TestbedAuthorizer.ts
@@ -9,11 +9,26 @@ import { leftTrim } from "../../utils/transformers";
  * @see: https://ioxio.com/guides/verify-id-token-in-a-data-source
  * @returns
  */
-async function getPublicKey(keyId: string): Promise<string> {
+async function getPublicKey(decodedToken: jwt.Jwt | null): Promise<{ pem: string; key: { kid: string; kty: string; use: string; alg: string; n: string; e: string } }> {
+  const keyId = decodedToken?.header.kid;
+  const payload = decodedToken?.payload;
+
+  // Verify input token
+  if (typeof keyId !== "string" || typeof payload !== "object" || payload === null) {
+    throw new Error("Bad request");
+  }
+
+  // Verify issuer pre-fetching the public key
+  const { iss } = payload;
+  if (iss !== "https://login.testbed.fi") {
+    throw new Error("Invalid issuer");
+  }
+
   const verifyConfig = await axios.get("https://login.testbed.fi/.well-known/openid-configuration");
   const jwksUri = verifyConfig.data.jwks_uri;
   const jwks = await axios.get(jwksUri);
-  return jwktopem(jwks.data.keys.find((key: any) => key.kid === keyId));
+  const key = jwks.data.keys.find((key: any) => key.kid === keyId);
+  return { pem: jwktopem(key), key: key };
 }
 
 /**
@@ -23,15 +38,13 @@ async function getPublicKey(keyId: string): Promise<string> {
  */
 export default async function authorize(idToken: string, context: string): Promise<void> {
   try {
+    // Decode token
     const token = leftTrim(idToken, "Bearer ");
+    const decodedToken = jwt.decode(token, { complete: true });
+
     // Validate token
-    const decoded = jwt.decode(token, { complete: true });
-    const keyId = decoded?.header.kid;
-    if (!keyId) {
-      throw new Error("No key id found");
-    }
-    const publicKey = await getPublicKey(keyId);
-    const verified = jwt.verify(token, publicKey);
+    const publicKey = await getPublicKey(decodedToken);
+    const verified = jwt.verify(token, publicKey.pem, { ignoreExpiration: false });
     debug(verified);
   } catch (error) {
     throw new AccessDeniedException(String(error));
