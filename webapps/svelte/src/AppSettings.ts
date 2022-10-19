61359f8d8ee93b878739b6509127d2c2832e5c24
Lassi Piironen <lsipii@kapsi.fi>
2022-10-19 13:08:23 +0300

    fix: revert app settings url change

--- webapps/svelte/src/AppSettings.ts
+++ webapps/svelte/src/AppSettings.ts
@@ -1,4 +1,4 @@
 export default {
   appName: "authflow-test",
-  authenticationGatewayHost: "https://q88uo5prmh.execute-api.eu-north-1.amazonaws.com",
+  authenticationGatewayHost: "http://localhost:3000",
 };
