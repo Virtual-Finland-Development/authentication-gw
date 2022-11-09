dc928cf8d3d496b528c7d5cd395bddbba9ca608a
Lassi Piironen <lsipii@kapsi.fi>
2022-11-09 20:03:32 +0200

    fix: update routes

--- src/routes/Saml2AuthRoutes.ts
+++ src/routes/Saml2AuthRoutes.ts
@@ -3,6 +3,6 @@ import { generateRequestHandlers } from "../utils/route-utils";
 
 const defaultAuthProviderIdent = SuomiFISettings.ident;
 const operationPrefix = "Saml2";
-const operationNames = ["LoginRequest", "AuthenticateResponse", "LogoutRequest", "LogoutResponse", "AuthTokenRequest", "UserInfoRequest", "WellKnownJWKSRequest"];
+const operationNames = ["LoginRequest", "AuthenticateResponse", "LoggedInRequest", "LogoutRequest", "LogoutResponse", "WellKnownJWKSRequest"];
 
 export default generateRequestHandlers(operationNames, operationPrefix, defaultAuthProviderIdent);
