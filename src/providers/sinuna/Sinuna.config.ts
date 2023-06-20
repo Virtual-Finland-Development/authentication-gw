1e2b24a96f5688262bc4ed8cbf00984a40038e3d
Lassi Piironen <lsipii@kapsi.fi>
2023-06-20 10:04:15 +0300

    chore: update sinuna scope to include persistent ID

--- src/providers/sinuna/Sinuna.config.ts
+++ src/providers/sinuna/Sinuna.config.ts
@@ -1,5 +1,5 @@
 // static SinunaSettings
 export default {
   ident: "sinuna", // The identifier for Sinuna
-  scope: "openid frontend sinuna_id", // The auth scope for Sinuna
+  scope: "openid frontend persistent_id", // The auth scope for Sinuna
 };
